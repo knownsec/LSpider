678f3920024994a0c01c2568884e9ab06d6e55df
LoRexxar <lorexxar@gmail.com>
2020-06-09 16:38:17 +0800

    fix stop

--- lspider_stop.sh
+++ lspider_stop.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 kill -2 $(ps aux | grep SpiderCoreBackendStart|grep -v grep|awk '{print $2}')
-kill -2 $(ps aux | grep SpiderCoreBackendStart|grep -v grep|awk '{print $2}')
-kill -2 $(ps aux | grep SpiderCoreBackendStart|grep -v grep|awk '{print $2}')
-kill -2 $(ps aux | grep SpiderCoreBackendStart|grep -v grep|awk '{print $2}')
-kill -2 $(ps aux | grep SpiderCoreBackendStart|grep -v grep|awk '{print $2}')
+sleep 1
+kiil -9 $(ps aux | grep chrome|grep -v grep|awk '{print $2}')
+kiil -9 $(ps aux | grep SpiderCoreBackendStart|grep -v grep|awk '{print $2}')
 
 #kill -2 $(ps aux | grep xray |grep -v grep|awk '{print $2}')
 
