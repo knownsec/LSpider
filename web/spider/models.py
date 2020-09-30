cb9f1d8c58201dad1a5037fbad16f05bc4fbccb7
LoRexxar <lorexxar@gmail.com>
2020-09-30 10:58:57 +0800

    add banner

--- web/spider/models.py
+++ web/spider/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class SubDomainList(models.Model):
     subdomain = models.CharField(max_length=200)
     title = models.CharField(max_length=1000, null=True, default="")
-    banner = models.CharField(max_length=1000)
+    banner = models.CharField(max_length=1000, null=True, default="")
     lastscan = models.DateTimeField()
     is_finished = models.BooleanField(default=False)
 
