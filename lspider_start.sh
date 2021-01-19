817c1d2de2145787a26f7c5c200377d8903b5959
LoRexxar <lorexxar@gmail.com>
2021-01-19 15:16:39 +0800

    update shell

--- lspider_start.sh
+++ lspider_start.sh
@@ -4,7 +4,7 @@ while :
 do
     if [ $(ps aux | grep SpiderCoreBackendStart|grep -v grep|wc -l) -eq 0 ];then
         echo "start"
-        /usr/bin/python3 /home/ubuntu/lorexxar/LSpider/manage.py SpiderCoreBackendStart
+        python3 $(cd "$(dirname "$0")";pwd)/manage.py SpiderCoreBackendStart
     fi
     sleep 100
 done
