20a4ad3b8135ccb8c04eaf3837858c4d08634aca
LoRexxar <lorexxar@gmail.com>
2021-02-02 16:40:40 +0800

    update shell script

--- lspider_start.sh
+++ lspider_start.sh
@@ -4,6 +4,7 @@ while :
 do
     if [ $(ps aux | grep SpiderCoreBackendStart|grep -v grep|wc -l) -eq 0 ];then
         echo "start"
+        chmod 644 $(cd "$(dirname "$0")";pwd)/vuls/*
         python3 $(cd "$(dirname "$0")";pwd)/manage.py SpiderCoreBackendStart
     fi
     sleep 100
--- lspider_stop.sh
+++ lspider_stop.sh
@@ -4,6 +4,3 @@ kill -2 $(ps aux | grep SpiderCoreBackendStart|grep -v grep|awk '{print $2}')
 sleep 3
 kill -9 $(ps aux | grep SpiderCoreBackendStart|grep -v grep|awk '{print $2}')
 kill -9 $(ps aux | grep chrome|grep -v grep|awk '{print $2}')
-
-
-chown www:www /home/wwwroot/default/xray/r*
